c5f0d79474f21fccbd41b18c69978afcaaeb8e74
Elijah <ekollie324@gmail.com>
2023-12-14 15:36:48 -0500

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import ipdb; 
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -12,4 +13,36 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, breed = "Mastiff", name = "Bruno"):
+        # print("Initializing")
+        self.breed = breed
+        self.name = name
+
+    def set_name(self, name):
+        # print("Setting name")
+        if type(name) != str:
+            print('Name must be string between 1 and 25 characters.')
+        elif 0 >= len(name) or 25 < len(name):
+            print('Name must be string between 1 and 25 characters.')
+        else: self._name = name
+
+    def get_name(self):
+        # print("Getting name")
+        return self._name
+
+    def get_breed(self):
+        # print("Getting Breed")
+        return self._breed
+
+    def set_breed(self, breed):
+        # print("Setting Breed")
+        if breed in APPROVED_BREEDS:
+            self._breed =  breed
+        else: print('Breed must be in list of approved breeds.')
+
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+
+# ipdb.set_trace()
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "No input", job = "No Input"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if type(name) != str:
+            print("Name must be string between 1 and 25 characters.")
+        elif 0 >= len(name) or len(name) > 25 :
+            print("Name must be string between 1 and 25 characters.")
+        else: self._name = name.title()
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if (job in APPROVED_JOBS) == False:
+            print("Job must be in list of approved jobs.")
+        else: self._job = job
+        
+        
+    
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
+    
